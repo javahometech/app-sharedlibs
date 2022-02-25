acc059fe0e4dac4009027bb50b9564ab4b889638
javahometech <kammana.hari@gmail.com>
2022-02-25 18:50:06 +0530

    Create calculatory.groovy

---
+++ vars/calculatory.groovy
@@ -0,0 +1,9 @@
+def add(x,y){
+  print "Sum of ${x} and ${y} is ${x+y}"
+}
+def sub(x,y){
+  print "substraction of ${x} and ${y} is ${x-y}"
+}
+def mul(x,y){
+  print "Multiplication of ${x} and ${y} is ${x*y}"
+}
