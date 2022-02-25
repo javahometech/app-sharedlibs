544bb6973cfb7a8feb4a437fc93bc3447ba0aebc
javahometech <kammana.hari@gmail.com>
2022-02-25 18:40:39 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,6 +1,7 @@
 def call(ips,user,credId){
   sshagent([credId]) {
     ips.each {
+      echo "deploying on ${it}"
       sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${it}:/opt/tomcat8/webapps/app.war"
       sh "ssh ${user}@${it} /opt/tomcat8/bin/shutdown.sh"
       sh "ssh ${user}@${it} /opt/tomcat8/bin/startup.sh"
